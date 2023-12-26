0c885d7dae193ea58e17dee784353caca020a4d5
Miguelgargor <140601367+Miguelgargor@users.noreply.github.com>
2023-12-26 16:42:11 +0100

    Update app_UCAV.py

--- app_UCAV.py
+++ app_UCAV.py
@@ -86,7 +86,7 @@ def funcion_filtrar_por_fecha(UCAV_PAGO_INGRESO, Nombre_Hoja, fecha_inicio_indic
 
     ### EN CASO DE ERROR-> Comprobar los parámetros:
     except Exception as e:
-        st.warning("**¡COMPRUEBE LOS PARÁMETROS INTRODUCIDOS!**: {}".format(str(e)))
+        st.warning("**¡COMPRUEBE LOS PARÁMETROS INTRODUCIDOS!**:", str(e))
 #############################################################################################################################
 
 ## A) CONFIGURACIÓN GENERAL DE LA PÁGINA WEB:
